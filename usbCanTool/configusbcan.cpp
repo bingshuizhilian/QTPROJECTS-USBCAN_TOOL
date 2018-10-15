a68d79ff88b159ec10a422aed8e89c97e27e5b72
BaiSong Yan <bingshuizhilian@yeah.net>
2018-10-15 17:09:35 +0800

    update

--- usbCanTool/configusbcan.cpp
+++ usbCanTool/configusbcan.cpp
@@ -6,23 +6,26 @@ ConfigUsbCan::ConfigUsbCan(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ConfigUsbCan)
 {
+    getUsbDeviceInfo();
+
     componentsAndLayoutInit();
 
-    //this->setGeometry(parent->geometry());
+    this->setGeometry(750,350,0,0);
 }
 
-ConfigUsbCan::~ConfigUsbCan()
+ConfigUsbCan::~ConfigUsbCan(void)
 {
     delete ui;
 }
 
-void ConfigUsbCan::componentsAndLayoutInit()
+void ConfigUsbCan::componentsAndLayoutInit(void)
 {
     ui->setupUi(this);
     //components
     usbDevList = new QComboBox;
-    okBtn = new QPushButton;
-    usbDevDescTbl = new QTableWidget;
+    okBtn = new QPushButton(tr("Apply"));
+    connect(usbDevList, &usbDevList->currentIndexChanged, this, &getUsbDeviceInfo);
+    usbDevDescTbl = new QTableWidget(14, 2);
 
     //Layout
     auto topLayout = new QHBoxLayout;
@@ -35,6 +38,58 @@ void ConfigUsbCan::componentsAndLayoutInit()
 
     this->setLayout(globleLayout);
     this->setWindowTitle("choose device");
-//    this->setGeometry();
 
 }
+
+int ConfigUsbCan::getUsbDeviceInfo(void)
+{
+    libusb_device **devs;
+    int r;
+    ssize_t cnt;
+    libusb_device *dev;
+    int i = 0, j = 0;
+    uint8_t path[8];
+
+    r = libusb_init(NULL);
+    if (r < 0)
+        return r;
+
+    cnt = libusb_get_device_list(NULL, &devs);
+    if (cnt < 0)
+        return (int) cnt;
+
+    while ((dev = devs[i++]) != NULL)
+    {
+        struct libusb_device_descriptor desc;
+        int r = libusb_get_device_descriptor(dev, &desc);
+        if (r < 0)
+        {
+            fprintf(stderr, "failed to get device descriptor");
+            return r;
+        }
+
+        if(desc.idVendor == USBCAN_VID && desc.idProduct == USBCAN_PID)
+            ;//usbDevHandle = devs[i];
+
+        printf("%04x:%04x (bus %d, device %d)",
+            desc.idVendor, desc.idProduct,
+            libusb_get_bus_number(dev), libusb_get_device_address(dev));
+
+        r = libusb_get_port_numbers(dev, path, sizeof(path));
+        if (r > 0)
+        {
+            printf(" path: %d", path[0]);
+            for (j = 1; j < r; j++)
+                printf(".%d", path[j]);
+        }
+        printf("\n");
+    }
+
+    libusb_free_device_list(devs, 1);
+
+    libusb_exit(NULL);
+
+    return 0;
+}
+
+
--- usbCanTool/configusbcan.h
+++ usbCanTool/configusbcan.h
@@ -1,6 +1,7 @@
 #ifndef CONFIGUSBCAN_H
 #define CONFIGUSBCAN_H
 
+#include "libusb.h"
 #include <QDialog>
 #include <QComboBox>
 #include <QPushButton>
@@ -16,19 +17,25 @@ class ConfigUsbCan : public QDialog
 
 public:
     explicit ConfigUsbCan(QWidget *parent = 0);
-    ~ConfigUsbCan();
+    ~ConfigUsbCan(void);
 
 private:
     Ui::ConfigUsbCan *ui;
 
 //added by me
 private:
+    const unsigned short USBCAN_VID = 0x4348;
+    const unsigned short USBCAN_PID = 0x5537;
+private:
+    libusb_device_handle *usbDevHandle;
+
     QComboBox *usbDevList;
     QPushButton *okBtn;
     QTableWidget *usbDevDescTbl;
 
 private:
-    void componentsAndLayoutInit();
+    void componentsAndLayoutInit(void);
+    int getUsbDeviceInfo(void);
 };
 
 #endif // CONFIGUSBCAN_H
